b436119c264dc2c5f60c9a9367e0f9a43879ceb0
SamSamsung <76557309+SamSamsung@users.noreply.github.com>
2022-11-27 17:23:33 +0100

    Add files via upload

--- graphique.py
+++ graphique.py
@@ -1,18 +1,54 @@
-def repere_orthonorme(n, fonction):
+def repere_ortho_i(i, n):
+    if i == n // 2:
+        i = 0
+    elif i < (n // 2):
+        i = 0 - (n // 2 - i)
+
+    elif i > n // 2:
+        i = i - n // 2
+    return i
+
+
+def repere_ortho_j(j, n):
+    if j == n // 2:
+        j = 0
+    elif j < n // 2:
+        j = 0 - (n // 2 - j)
+
+    elif j > n // 2:
+        j = j - n // 2
+    return j
+
+
+def dessiner_repere(i, j, L, x, y):
+    if x == 0:
+        L[i][j] = 1
+    elif y == 0:
+        L[i][j] = 2
+    else:
+        L[i][j] = 0
+    return L
+
+
+def dessiner_fonction(i, j, L, x, y, equation):
+    if x == -(eval(equation)):
+        if L[i][j] == 1:
+            L[i][j] = 1
+        elif L[i][j] == 2:
+            L[i][j] = 2
+        else:
+            L[i][j] = 3
+    return L
+
+
+def carre(n, equation):
     L = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(n):
-        y = (n//2) - i
+        x = repere_ortho_i(i, n)
         for j in range(n):
-            x = (n//2) - j
-            try:
-                if y == eval(fonction):
-                    L[i][j] = 3
-            except ZeroDivisionError:
-                L[i][j] = 0
-            if x == 0:
-                L[i][j] = 2
-            elif y == 0:
-                L[i][j] = 1
+            y = repere_ortho_j(j, n)
+            L = dessiner_repere(i, j, L, x, y)
+            L = dessiner_fonction(i, j, L, x, y, equation)
     return L
 
 
@@ -30,18 +66,15 @@ def affichage(L):
         print()
 
 
-
-
-
 exit = True
 
 print("Bienvenue sur le graphique")
 print("Veuillez poser votre equation tout en espaçant les calculs : ")
 print("2 * x + 3")
 
-
 while exit:
     equa = str(input("Veuillez écrire l'équation : "))
     if equa == "exit":
         exit = False
-    affichage(repere_orthonorme(40, equa))
+    equa = equa.replace("x", "y")
+    affichage(carre(40, equa))
